e057474bbe8b88096188c4cf3131f2fb9820407c
Lukas Knuth <mr.luke.187@googlemail.com>
2018-06-08 22:33:31 +0200

    Added Bootstrap as a dependency.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import BootstrapVue from "bootstrap-vue";
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
