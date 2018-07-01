234775ec4ed964129bd62ae656bd4e231b95407a
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-07-01 15:10:42 +0200

    Rename default experimental lvl for clarity
    Now it is harder to confuse it with an actual level.

--- src/content/lessons/maze/levels/TestLevel.ts
+++ src/content/lessons/maze/levels/TestLevel.ts
@@ -8,7 +8,7 @@ import {
 } from '@/components/game/GameState';
 import {API, default as Interpreter, InterpreterScope} from 'js-interpreter';
 
-export default class MazeLevel1 implements Level {
+export default class TestLevel implements Level {
     public readonly name = "Level 1";
     public readonly description = "A test level about mazes!";
     public maxBlocks = Infinity;
