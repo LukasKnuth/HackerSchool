cfb3cd79b92284a0b53950ae0eb8dd3d588c4b34
Lukas Knuth <knuth@imagineon.de>
2018-07-04 14:11:16 +0200

    Added level-progress information to the VueX store.

--- src/store/CourseProgress.ts
+++ src/store/CourseProgress.ts
@@ -3,41 +3,92 @@ import {RootState} from "@/store";
 import MazeLesson from "@/content/lessons/maze/MazeLesson";
 import {Lesson, Level} from "@/content/Lesson";
 
-export const ACTION_SELECT_LESSON = "selectLesson";
-export const ACTION_SELECT_LEVEL = "selectLevel";
-
-interface CourseProgressState {
-    currentLesson?: string,
-    currentLevel?: number
-}
-type Context = ActionContext<CourseProgressState, RootState>;
-
-type LessonList = {id: string, lesson: Lesson}[];
-
+// --------- STATIC DATA -------------
 const AllLessons: {[index: string]: Lesson} = {
     maze: new MazeLesson()
 };
+
+// -------- MODULE -------------
+export const ACTION_SELECT_LESSON = "selectLesson";
+export const ACTION_SELECT_LEVEL = "selectLevel";
+export const ACTION_SET_LEVEL_PROGRESS = "setLevelProgress";
+
+interface LevelProgress {
+    isFinished: boolean;
+    // TODO extend this to hold workspace-xml, etz.
+}
+interface LessonProgress {
+    levelProgress: {
+        [index: number]: LevelProgress
+    }
+}
+interface CourseProgressState {
+    currentLesson?: string,
+    currentLevel?: number,
+    lessonProgress: {
+        [index: string]: LessonProgress
+    }
+}
+type Context = ActionContext<CourseProgressState, RootState>;
+export type LessonListEntry = {
+    id: string,
+    lesson: Lesson,
+    hasProgress: boolean,
+    isFinished: boolean
+};
+
+interface LevelProgressPayload {
+    lessonId: string,
+    levelNr: number,
+    progress: LevelProgress
+}
+
+function getLessonProgress(state: CourseProgressState, lessonId: string): LessonProgress|undefined {
+    if (lessonId in state.lessonProgress) {
+        return state.lessonProgress[lessonId];
+    } else {
+        return undefined;
+    }
+}
+function getLevelProgress(state: CourseProgressState, lessonId: string, level: number): LevelProgress|undefined {
+    const lessonProgress = getLessonProgress(state, lessonId);
+    if (lessonProgress && level in lessonProgress.levelProgress) {
+        return lessonProgress.levelProgress[level];
+    }
+    return undefined;
+}
+function hasLevelProgress(state: CourseProgressState, lessonId: string, level: number): boolean {
+    return getLevelProgress(state, lessonId, level) !== undefined;
+}
+function isLevelFinished(state: CourseProgressState, lessonId: string, level: number): boolean {
+    const progress = getLevelProgress(state, lessonId, level);
+    return progress ? progress.isFinished : false;
+}
 
 const CourseProgressModule: Module<CourseProgressState, RootState> = {
     state: () => {
         return {
             currentLevel: undefined,
-            currentLesson: undefined
+            currentLesson: undefined,
+            lessonProgress: {}
         }
     },
     mutations: { // These shouldn't be used directly, as they allow setting invalid state...
         setLesson: (state: CourseProgressState, lessonId: string) => {
             state.currentLesson = lessonId;
-            state.currentLevel = 0;
         },
         setLevel: (state: CourseProgressState, levelNr: number) => {
             state.currentLevel = levelNr;
+        },
+        setLevelProgress: (state: CourseProgressState, payload: LevelProgressPayload) => {
+            state.lessonProgress[payload.lessonId].levelProgress[payload.levelNr] = payload.progress;
         }
     },
     actions: {
         [ACTION_SELECT_LESSON]: (context: Context, lessonId: string) => {
             if (lessonId in AllLessons) {
                 context.commit("setLesson", lessonId);
+                context.commit("setLevel", 0); // TODO find first uncompleted level, set that.
             } else {
                 console.warn(`Unknown lesson-id "${lessonId}", ignoring...`);
             }
@@ -55,12 +106,26 @@ const CourseProgressModule: Module<CourseProgressState, RootState> = {
             } else {
                 console.warn("Can't set the level yet, no lesson was selected!");
             }
+        },
+        [ACTION_SET_LEVEL_PROGRESS]: (context: Context, progress: LevelProgress) => {
+            if (context.state.currentLesson && context.state.currentLevel) {
+                context.commit("setLevelProgress", {
+                    lessonId: context.state.currentLesson,
+                    levelNr: context.state.currentLevel,
+                    progress
+                });
+            } else {
+                console.warn("Can't set level progress because no level/lesson is selected!");
+            }
         }
     },
     getters: {
-        lessons: (): LessonList => {
+        lessons: (state: CourseProgressState): LessonListEntry[] => {
             return Object.keys(AllLessons).map((id: string) => {
-                return {id, lesson: AllLessons[id]}
+                const lesson = AllLessons[id];
+                const progress = lesson.getLevels().some((_, i) => hasLevelProgress(state, id, i));
+                const finished = lesson.getLevels().every((_, i) => isLevelFinished(state, id, i));
+                return {id, lesson, hasProgress: progress, isFinished: finished};
             });
         },
         currentLesson: (state: CourseProgressState): Lesson|undefined => {
