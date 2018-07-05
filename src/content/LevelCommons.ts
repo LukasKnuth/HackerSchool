3aa0b6c355f92083276cbd6251fc75ecbcf2b411
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-07-06 01:29:37 +0200

    Add commonly used strings for level messages

---
+++ src/content/LevelCommons.ts
@@ -0,0 +1,3 @@
+export const MSG_FALLEN_IN_PIT = "Du bist in den Abgrund gestürzt!";
+export const MSG_FALLEN_IN_TRAP = "Du bist in eine Falle gelaufen!";
+export const MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED = "Dein Code ist zuende aber der Roboter hat den Kristall noch nicht eingesammelt!";
