265dc733f6d3db64cad58b2dd834dd33abb0c04f
Lukas Knuth <mr.luke.187@googlemail.com>
2018-07-05 23:26:22 +0200

    Added localStorage browser persistence with VueX persist library.

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,25 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 import CourseProgressModule from "@/store/CourseProgress";
 
 Vue.use(Vuex);
 
 export interface RootState {}
 
+const vuexLocal = new VuexPersistence({
+    strictMode: process.env.NODE_ENV !== 'production',
+    storage: window.localStorage
+});
+
 export default new Vuex.Store<RootState>({
     strict: process.env.NODE_ENV !== 'production',
     state: {},
     modules: {
         CourseProgressModule
-    }
+    },
+    mutations: {
+        RESTORE_MUTATION: vuexLocal.RESTORE_MUTATION // needed to restore state from localStorage
+    },
+    plugins: [vuexLocal.plugin]
 });
