47a19b737a0bdcdfbfadbc02010760ac50add8e0
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-07-06 05:15:55 +0200

    Implement lvl 13 logic

--- src/content/lessons/levels/Lvl13_NumericVariables.ts
+++ src/content/lessons/levels/Lvl13_NumericVariables.ts
@@ -1,19 +1,28 @@
 import {BlockToolbox, Level} from '@/content/Lesson';
 import {
+    _,
     GameState,
-    GridPosition, GridState, _, PlayerPosition,
-    T, X, PLAYER_ORIENTATION_RIGHT, PLAYER_ORIENTATION_UP, B, PLAYER_ORIENTATION_LEFT
+    GridState,
+    PLAYER_ORIENTATION_LEFT,
+    PlayerPosition,
+    T,
+    TILE_COLLECTIBLE,
+    TILE_PIT,
+    X
 } from '@/components/game/GameState';
 import {API, default as Interpreter, InterpreterScope} from 'js-interpreter';
+import {attachGameBlockAPI} from "@/content/blocks/API";
+import {MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED, MSG_FALLEN_IN_PIT} from "@/content/LevelCommons";
 
 export default class Lvl13_NumericVariables implements Level {
     public readonly allowMethods = false;
-    public readonly allowVariables = false;
+    public readonly allowVariables = true;
+    public readonly showDebugLog = true;
     public readonly description = "Auch Zahlen können gespeichert werden.";
-    public readonly maxBlocks = 6;
+    public readonly maxBlocks = 10;
     public readonly mazeHeight = 15;
     public readonly mazeWidth = 15;
-    public readonly name = "Level 12 - Numerische Variabeln";
+    public readonly name = "Numerische Variabeln";
 
     private initialMazeLayout: GridState = [
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
@@ -34,11 +43,19 @@ export default class Lvl13_NumericVariables implements Level {
     ];
 
     exportAPI(gameState: GameState) {
-        return undefined;
+        return (interpreter: Interpreter, scope: InterpreterScope) => {
+            attachGameBlockAPI(interpreter, scope, gameState);
+        };
     }
 
     getBlocks(): BlockToolbox {
-        return undefined;//variables of bool and of int should be possible. All numeric operations except for comparisons in conditionals should be available
+        return {
+            Control: ["controls_if", "controls_repeat_ext"/*, "controls_whileUntil"*/],
+            Game: ["forward", "backward", 'turn_left', 'turn_right', 'sensor_camera'/*, 'sensor_radar'*/],
+            //   Logic: ["logic_compare", "logic_operation", "logic_negate", "math_arithmetic", "math_modulo"],
+            Values: ["math_number"/*, "text", "logic_boolean", "math_random_int"*/],
+            //   Debug: ["debug_log"]
+        };
     }
 
     initializeState(gameState: GameState): void {
@@ -47,5 +64,20 @@ export default class Lvl13_NumericVariables implements Level {
     }
 
     tick(gameState: GameState): void {
+        const player = gameState.getPlayerPosition();
+        const standingOn = gameState.getGridTile(player);
+        switch (standingOn) {
+            case TILE_PIT:
+                gameState.setGameOver(true, false, MSG_FALLEN_IN_PIT);
+                break;
+            case TILE_COLLECTIBLE:
+                // Instantly wins game
+                gameState.setGameOver(true, true, "Super! Was glaubst du ist die größte Zahl, die du speichern kannst? Warum? Prüfe deine Vermutung nach.");
+                break;
+        }
+
+        if(!gameState.isGameOver && !gameState.hasMoreCode){
+            gameState.setGameOver(true, false, MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED)
+        }
     }
 }
