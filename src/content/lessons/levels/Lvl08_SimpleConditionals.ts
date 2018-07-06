a29fda157a8ce53eed0ee0d9bcc476524b446588
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-07-06 03:11:17 +0200

    Implement lvl 8 logic

--- src/content/lessons/levels/Lvl08_SimpleConditionals.ts
+++ src/content/lessons/levels/Lvl08_SimpleConditionals.ts
@@ -1,28 +1,37 @@
 import {BlockToolbox, Level} from '@/content/Lesson';
 import {
+    _,
     GameState,
-    GridPosition, GridState, _, PlayerPosition,
-    T, X, PLAYER_ORIENTATION_RIGHT, PLAYER_ORIENTATION_UP
+    GridState,
+    PLAYER_ORIENTATION_RIGHT,
+    PlayerPosition,
+    T,
+    TILE_COLLECTIBLE,
+    TILE_PIT,
+    X
 } from '@/components/game/GameState';
 import {API, default as Interpreter, InterpreterScope} from 'js-interpreter';
+import {MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED, MSG_FALLEN_IN_PIT} from "@/content/LevelCommons";
+import {attachGameBlockAPI} from "@/content/blocks/API";
 
 export default class Lvl08_SimpleConditionals implements Level {
     public readonly allowMethods = false;
     public readonly allowVariables = false;
+    public readonly showDebugLog = true;
     public readonly description = "Mit der neuen Kamera kann dein Roboter das Feld vor ihm untersuchen. Zusammen mit den " +
         "bedingten Anweisungen, kann er jetzt auf die Umwelt reagieren.";
-    public readonly maxBlocks = 4;
+    public readonly maxBlocks = 6;
     public readonly mazeHeight = 15;
     public readonly mazeWidth = 15;
-    public readonly name = "Level 8 - Simple Bedingungen";
+    public readonly name = "Bedingungen";
 
     private initialMazeLayout: GridState = [
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
-        [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
         [X, X, X, X, X, X, T, X, X, X, X, X, X, X, X],
+        [X, X, X, X, X, X, _, X, X, X, X, X, X, X, X],
         [X, X, X, X, X, X, _, X, X, X, X, X, X, X, X],
         [X, X, X, _, _, _, _, X, X, X, X, X, X, X, X],
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
@@ -35,11 +44,19 @@ export default class Lvl08_SimpleConditionals implements Level {
     ];
 
     exportAPI(gameState: GameState) {
-        return undefined;
+        return (interpreter: Interpreter, scope: InterpreterScope) => {
+            attachGameBlockAPI(interpreter, scope, gameState);
+        };
     }
 
     getBlocks(): BlockToolbox {
-        return undefined;//only `simple if` for this level
+        return {
+            Control: ["controls_if", "controls_repeat_ext"/*, "controls_whileUntil"*/],
+            Game: ["forward", "backward", 'turn_left', 'turn_right', 'sensor_camera'/*, 'sensor_radar'*/],
+            //   Logic: ["logic_compare", "logic_operation", "logic_negate", "math_arithmetic", "math_modulo"],
+            Values: ["math_number"/*, "text", "logic_boolean", "math_random_int"*/],
+            //   Debug: ["debug_log"]
+        };
     }
 
     initializeState(gameState: GameState): void {
@@ -48,5 +65,21 @@ export default class Lvl08_SimpleConditionals implements Level {
     }
 
     tick(gameState: GameState): void {
+        const player = gameState.getPlayerPosition();
+        const standingOn = gameState.getGridTile(player);
+        switch (standingOn) {
+            case TILE_PIT:
+                gameState.setGameOver(true, false, MSG_FALLEN_IN_PIT);
+                break;
+            case TILE_COLLECTIBLE:
+                // Instantly wins game
+                gameState.setGameOver(true, true, "Mach weiter so!");
+                break;
+        }
+
+        if(!gameState.isGameOver && !gameState.hasMoreCode){
+            gameState.setGameOver(true, false, MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED)
+        }
     }
+
 }
