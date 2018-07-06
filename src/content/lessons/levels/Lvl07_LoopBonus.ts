3d80ac3dfdffb046e2b2580c930a3ff6b8eaefff
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-07-06 02:55:33 +0200

    Implement lvl 7 logic

--- src/content/lessons/levels/Lvl07_LoopBonus.ts
+++ src/content/lessons/levels/Lvl07_LoopBonus.ts
@@ -1,19 +1,28 @@
 import {BlockToolbox, Level} from '@/content/Lesson';
 import {
+    _,
     GameState,
-    GridPosition, GridState, _, PlayerPosition,
-    T, X, PLAYER_ORIENTATION_RIGHT, PLAYER_ORIENTATION_UP
+    GridState,
+    PLAYER_ORIENTATION_UP,
+    PlayerPosition,
+    T,
+    TILE_COLLECTIBLE,
+    TILE_PIT,
+    X
 } from '@/components/game/GameState';
 import {API, default as Interpreter, InterpreterScope} from 'js-interpreter';
+import {attachGameBlockAPI} from "@/content/blocks/API";
+import {MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED, MSG_FALLEN_IN_PIT} from "@/content/LevelCommons";
 
 export default class Lvl07_LoopBonus implements Level {
     public readonly allowMethods = false;
     public readonly allowVariables = false;
+    public readonly showDebugLog = false;
     public readonly description = "Eine ganz besondere Herausforderung";
-    public readonly maxBlocks = 22; //Actually 19 should be possible
+    public readonly maxBlocks = 30; //26 definitely possible
     public readonly mazeHeight = 15;
     public readonly mazeWidth = 15;
-    public readonly name = "Level 7 - EXTRA Schleifen";
+    public readonly name = "Schleifen - BONUS";
 
     private initialMazeLayout: GridState = [
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
@@ -35,11 +44,19 @@ export default class Lvl07_LoopBonus implements Level {
 
 
     exportAPI(gameState: GameState) {
-        return undefined;
+        return (interpreter: Interpreter, scope: InterpreterScope) => {
+            attachGameBlockAPI(interpreter, scope, gameState);
+        };
     }
 
     getBlocks(): BlockToolbox {
-        return undefined;//only `repeat X times` block from repeat section
+        return {
+            Control: [/*"controls_if",*/ "controls_repeat_ext"/*, "controls_whileUntil"*/],
+            Game: ["forward", "backward", 'turn_left', 'turn_right'/*, 'sensor_camera', 'sensor_radar'*/],
+            //   Logic: ["logic_compare", "logic_operation", "logic_negate", "math_arithmetic", "math_modulo"],
+            Values: ["math_number"/*, "text", "logic_boolean", "math_random_int"*/],
+            //   Debug: ["debug_log"]
+        };
     }
 
     initializeState(gameState: GameState): void {
@@ -48,5 +65,20 @@ export default class Lvl07_LoopBonus implements Level {
     }
 
     tick(gameState: GameState): void {
+        const player = gameState.getPlayerPosition();
+        const standingOn = gameState.getGridTile(player);
+        switch (standingOn) {
+            case TILE_PIT:
+                gameState.setGameOver(true, false, MSG_FALLEN_IN_PIT);
+                break;
+            case TILE_COLLECTIBLE:
+                // Instantly wins game
+                gameState.setGameOver(true, true, "Du hast es geschafft! Du kannst immer wieder zu den gelösten Leveln zurückkehren und versuchen eine andere Lösung zu finden.");
+                break;
+        }
+
+        if(!gameState.isGameOver && !gameState.hasMoreCode){
+            gameState.setGameOver(true, false, MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED)
+        }
     }
 }
