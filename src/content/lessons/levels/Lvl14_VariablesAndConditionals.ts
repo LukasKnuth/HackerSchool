df7dea219e292e57641896eea17ee94a74f3f701
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-07-06 05:48:05 +0200

    Implement lvl 14 logic

--- src/content/lessons/levels/Lvl14_VariablesAndConditionals.ts
+++ src/content/lessons/levels/Lvl14_VariablesAndConditionals.ts
@@ -1,19 +1,32 @@
 import {BlockToolbox, Level} from '@/content/Lesson';
 import {
+    _,
+    B,
     GameState,
-    GridPosition, GridState, _, PlayerPosition,
-    T, X, PLAYER_ORIENTATION_RIGHT, PLAYER_ORIENTATION_UP, B, PLAYER_ORIENTATION_LEFT
+    GridState,
+    PLAYER_ORIENTATION_UP,
+    PlayerPosition,
+    T,
+    TILE_COLLECTIBLE,
+    TILE_NEUTRAL,
+    TILE_PIT,
+    X
 } from '@/components/game/GameState';
 import {API, default as Interpreter, InterpreterScope} from 'js-interpreter';
+import {attachGameBlockAPI} from "@/content/blocks/API";
+import {MSG_CODE_OVER_BUT_YOU_NEED_MORE_COLLECTABLES, MSG_FALLEN_IN_PIT} from "@/content/LevelCommons";
 
 export default class Lvl14_VariablesAndConditionals implements Level {
     public readonly allowMethods = false;
-    public readonly allowVariables = false;
-    public readonly description = "";
-    public readonly maxBlocks = 11;
+    public readonly allowVariables = true;
+    public readonly showDebugLog = true;
+    public readonly description = "Zum Rechnen und Vergleichen von Zahlen stehen dir neue neue Blöcke zur Verfügung. Dein Roboter kann jetzt also zählen.";
+    public readonly maxBlocks = 14;
     public readonly mazeHeight = 15;
     public readonly mazeWidth = 15;
-    public readonly name = "Level 14 - Variabeln und Bedingungen";
+    public readonly name = "Vergleiche und Berechnungen";
+    private requiredCollectablesNumber = 2;
+    private currentlyAcquiredCollectibles = 0;
 
     private initialMazeLayout: GridState = [
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
@@ -23,7 +36,7 @@ export default class Lvl14_VariablesAndConditionals implements Level {
         [X, X, X, _, X, B, X, _, X, _, X, _, X, X, X],
         [X, X, _, _, _, _, _, _, _, _, _, B, _, X, X],
         [X, X, X, _, X, B, X, _, X, _, X, _, X, X, X],
-        [X, X, _, _, _, _, _, _, _, _, _, _, _, X, X],
+        [X, X, _, _, _, _, _, _, _, _, _, B, _, X, X],
         [X, X, X, _, X, B, X, _, X, _, X, B, X, X, X],
         [X, X, B, B, B, B, _, _, B, T, _, B, _, X, X],
         [X, X, X, _, X, _, X, _, X, _, X, B, X, X, X],
@@ -34,18 +47,46 @@ export default class Lvl14_VariablesAndConditionals implements Level {
     ];
 
     exportAPI(gameState: GameState) {
-        return undefined;
+        return (interpreter: Interpreter, scope: InterpreterScope) => {
+            attachGameBlockAPI(interpreter, scope, gameState);
+        };
     }
 
     getBlocks(): BlockToolbox {
-        return undefined;//Comparisons with >=, <=, ==, <, > should now be available
+        return {
+            Control: ["controls_if", "controls_repeat_ext"/*, "controls_whileUntil"*/],
+            Game: ["forward", "backward", 'turn_left', 'turn_right', 'sensor_camera'/*, 'sensor_radar'*/],
+            Logic: ["logic_compare", /*"logic_operation", "logic_negate",*/ "math_arithmetic", "math_modulo"],
+            Values: ["math_number"/*, "text", "logic_boolean", "math_random_int"*/],
+            //   Debug: ["debug_log"]
+        };
     }
 
     initializeState(gameState: GameState): void {
+        this.currentlyAcquiredCollectibles = 0;
         gameState.setGridState(this.initialMazeLayout);
         gameState.setPlayerPosition(new PlayerPosition(5, 12, PLAYER_ORIENTATION_UP));
     }
 
-    tick(gameState: GameState): void {//make sure to remind players of having to collect both collectibles
+    tick(gameState: GameState): void {
+        const player = gameState.getPlayerPosition();
+        const standingOn = gameState.getGridTile(player);
+        switch (standingOn) {
+            case TILE_PIT:
+                gameState.setGameOver(true, false, MSG_FALLEN_IN_PIT);
+                break;
+            case TILE_COLLECTIBLE:
+                this.currentlyAcquiredCollectibles ++;
+                if (this.currentlyAcquiredCollectibles >= this.requiredCollectablesNumber) {
+                    gameState.setGameOver(true, true, "Clever! Jetzt stehen dir verschiedene Arten von Variabeln und Vergleichen zur Verfügung. Entscheide geschickt, wie du Informationen speicherst und nutzt.");
+                }else{
+                    gameState.setGridTile(player, TILE_NEUTRAL);
+                }
+                break;
+        }
+
+        if(!gameState.isGameOver && !gameState.hasMoreCode){
+            gameState.setGameOver(true, false, `${MSG_CODE_OVER_BUT_YOU_NEED_MORE_COLLECTABLES} (${this.currentlyAcquiredCollectibles} von ${this.requiredCollectablesNumber})`)
+        }
     }
 }
