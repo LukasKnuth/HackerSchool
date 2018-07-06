2b7382298782b90bdf2af3e0e3a5427debbf8f49
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-07-06 03:35:06 +0200

    Implement lvl 9 logic

--- src/content/lessons/levels/Lvl09_SimpleConditionalsWithATwist.ts
+++ src/content/lessons/levels/Lvl09_SimpleConditionalsWithATwist.ts
@@ -1,19 +1,28 @@
 import {BlockToolbox, Level} from '@/content/Lesson';
 import {
+    _,
     GameState,
-    GridPosition, GridState, _, PlayerPosition,
-    T, X, PLAYER_ORIENTATION_RIGHT, PLAYER_ORIENTATION_UP
+    GridState,
+    PLAYER_ORIENTATION_RIGHT,
+    PlayerPosition,
+    T,
+    TILE_COLLECTIBLE,
+    TILE_PIT,
+    X
 } from '@/components/game/GameState';
 import {API, default as Interpreter, InterpreterScope} from 'js-interpreter';
+import {attachGameBlockAPI} from "@/content/blocks/API";
+import {MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED, MSG_FALLEN_IN_PIT} from "@/content/LevelCommons";
 
 export default class Lvl09_SimpleConditionalsWithATwist implements Level {
     public readonly allowMethods = false;
     public readonly allowVariables = false;
+    public readonly showDebugLog = true;
     public readonly description = "";
-    public readonly maxBlocks = 5;
+    public readonly maxBlocks = 7;
     public readonly mazeHeight = 15;
     public readonly mazeWidth = 15;
-    public readonly name = "Level 9 - Bedingungen für Fortgeschrittene";
+    public readonly name = "Bedingungen für Fortgeschrittene";
 
     private initialMazeLayout: GridState = [
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
@@ -34,11 +43,19 @@ export default class Lvl09_SimpleConditionalsWithATwist implements Level {
     ];
 
     exportAPI(gameState: GameState) {
-        return undefined;
+        return (interpreter: Interpreter, scope: InterpreterScope) => {
+            attachGameBlockAPI(interpreter, scope, gameState);
+        };
     }
 
     getBlocks(): BlockToolbox {
-        return undefined;//only `simple if` for this level
+        return {
+            Control: ["controls_if", "controls_repeat_ext"/*, "controls_whileUntil"*/],
+            Game: ["forward", "backward", 'turn_left', 'turn_right', 'sensor_camera'/*, 'sensor_radar'*/],
+            //   Logic: ["logic_compare", "logic_operation", "logic_negate", "math_arithmetic", "math_modulo"],
+            Values: ["math_number"/*, "text", "logic_boolean", "math_random_int"*/],
+            //   Debug: ["debug_log"]
+        };
     }
 
     initializeState(gameState: GameState): void {
@@ -47,5 +64,20 @@ export default class Lvl09_SimpleConditionalsWithATwist implements Level {
     }
 
     tick(gameState: GameState): void {
+        const player = gameState.getPlayerPosition();
+        const standingOn = gameState.getGridTile(player);
+        switch (standingOn) {
+            case TILE_PIT:
+                gameState.setGameOver(true, false, MSG_FALLEN_IN_PIT);
+                break;
+            case TILE_COLLECTIBLE:
+                // Instantly wins game
+                gameState.setGameOver(true, true, "Das war knifflig!");
+                break;
+        }
+
+        if(!gameState.isGameOver && !gameState.hasMoreCode){
+            gameState.setGameOver(true, false, MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED)
+        }
     }
 }
