c80e56b9587b9032b10fe43a4c2a0ab2b8a62370
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-07-06 02:15:28 +0200

    Implement lvl 5 logic

--- src/content/lessons/levels/Lvl05_LoopWithOuterCode.ts
+++ src/content/lessons/levels/Lvl05_LoopWithOuterCode.ts
@@ -1,19 +1,28 @@
 import {BlockToolbox, Level} from '@/content/Lesson';
 import {
+    _,
     GameState,
-    GridPosition, GridState, _, PlayerPosition,
-    T, X, PLAYER_ORIENTATION_RIGHT, PLAYER_ORIENTATION_UP
+    GridState,
+    PLAYER_ORIENTATION_DOWN,
+    PlayerPosition,
+    T,
+    TILE_COLLECTIBLE,
+    TILE_PIT,
+    X
 } from '@/components/game/GameState';
 import {API, default as Interpreter, InterpreterScope} from 'js-interpreter';
+import {attachGameBlockAPI} from "@/content/blocks/API";
+import {MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED, MSG_FALLEN_IN_PIT} from "@/content/LevelCommons";
 
 export default class Lvl05_LoopWithOuterCode implements Level {
     public readonly allowMethods = false;
     public readonly allowVariables = false;
+    public readonly showDebugLog = false;
     public readonly description = "";
-    public readonly maxBlocks = 15;
+    public readonly maxBlocks = 16;
     public readonly mazeHeight = 15;
     public readonly mazeWidth = 15;
-    public readonly name = "Level 5 - Komplexe Schleifen";
+    public readonly name = "Komplexe Schleifen";
 
     private initialMazeLayout: GridState = [
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
@@ -35,18 +44,41 @@ export default class Lvl05_LoopWithOuterCode implements Level {
 
 
     exportAPI(gameState: GameState) {
-        return undefined;
+        return (interpreter: Interpreter, scope: InterpreterScope) => {
+            attachGameBlockAPI(interpreter, scope, gameState);
+        };
     }
 
     getBlocks(): BlockToolbox {
-        return undefined;//only `repeat X times` block from repeat section
+        return {
+            Control: [/*"controls_if",*/ "controls_repeat_ext"/*, "controls_whileUntil"*/],
+            Game: ["forward", "backward", 'turn_left', 'turn_right'/*, 'sensor_camera', 'sensor_radar'*/],
+            //   Logic: ["logic_compare", "logic_operation", "logic_negate", "math_arithmetic", "math_modulo"],
+            Values: ["math_number"/*, "text", "logic_boolean", "math_random_int"*/],
+            //   Debug: ["debug_log"]
+        };
     }
 
     initializeState(gameState: GameState): void {
         gameState.setGridState(this.initialMazeLayout);
-        gameState.setPlayerPosition(new PlayerPosition(1, 3, PLAYER_ORIENTATION_UP));
+        gameState.setPlayerPosition(new PlayerPosition(2, 2, PLAYER_ORIENTATION_DOWN));
     }
 
     tick(gameState: GameState): void {
+        const player = gameState.getPlayerPosition();
+        const standingOn = gameState.getGridTile(player);
+        switch (standingOn) {
+            case TILE_PIT:
+                gameState.setGameOver(true, false, MSG_FALLEN_IN_PIT);
+                break;
+            case TILE_COLLECTIBLE:
+                // Instantly wins game
+                gameState.setGameOver(true, true, "Super! Halte immer wieder Ausschau nach sich wiederholenden Mustern, die durch Schleifen vereinfacht werden können.");
+                break;
+        }
+
+        if(!gameState.isGameOver && !gameState.hasMoreCode){
+            gameState.setGameOver(true, false, MSG_CODE_OVER_BUT_COLLECTABLE_NOT_GATHERED)
+        }
     }
 }
