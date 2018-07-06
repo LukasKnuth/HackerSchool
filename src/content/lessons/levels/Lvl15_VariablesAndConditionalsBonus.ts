719cab82b8f05bedbe01c837b6cf31943b8abf1a
Yuliy Khorolinskyy <yk.email@yahoo.de>
2018-07-06 06:22:54 +0200

    Implement lvl 15 logic

--- src/content/lessons/levels/Lvl15_VariablesAndConditionalsBonus.ts
+++ src/content/lessons/levels/Lvl15_VariablesAndConditionalsBonus.ts
@@ -1,19 +1,34 @@
 import {BlockToolbox, Level} from '@/content/Lesson';
 import {
+    B,
+    G,
     GameState,
-    GridPosition, GridState, _, PlayerPosition,
-    T, X, PLAYER_ORIENTATION_RIGHT, PLAYER_ORIENTATION_UP, B, PLAYER_ORIENTATION_LEFT, G
+    GridState,
+    PLAYER_ORIENTATION_RIGHT,
+    PlayerPosition,
+    T,
+    TILE_COLLECTIBLE,
+    TILE_NEUTRAL,
+    TILE_PIT,
+    X
 } from '@/components/game/GameState';
 import {API, default as Interpreter, InterpreterScope} from 'js-interpreter';
+import {attachGameBlockAPI} from "@/content/blocks/API";
+import {MSG_CODE_OVER_BUT_YOU_NEED_MORE_COLLECTABLES, MSG_FALLEN_IN_PIT} from "@/content/LevelCommons";
 
 export default class Lvl15_VariablesAndConditionalsBonus implements Level {
     public readonly allowMethods = false;
-    public readonly allowVariables = false;
-    public readonly description = "";
-    public readonly maxBlocks = 15; //verify that number
+    public readonly allowVariables = true;
+    public readonly showDebugLog = true;
+    public readonly description = "So eine bunte Landschaft! Zum Glück hast du jetzt neue Blöcke um Wahrheitswerte zu kombinieren.";
+    public readonly maxBlocks = 18;//is possible to solve in 13
     public readonly mazeHeight = 15;
     public readonly mazeWidth = 15;
-    public readonly name = "Level 15 - EXTRA Variabeln und Bedingungen";
+    public readonly name = "Verzwickte Verzweigungen - BONUS";
+
+    private currentlyAcquiredCollectibles = 0;
+    private requiredCollectablesNumber = 8;
+
 
     private initialMazeLayout: GridState = [
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
@@ -32,19 +47,48 @@ export default class Lvl15_VariablesAndConditionalsBonus implements Level {
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
         [X, X, X, X, X, X, X, X, X, X, X, X, X, X, X],
     ];
+
     exportAPI(gameState: GameState) {
-        return undefined;
+        return (interpreter: Interpreter, scope: InterpreterScope) => {
+            attachGameBlockAPI(interpreter, scope, gameState);
+        };
     }
 
     getBlocks(): BlockToolbox {
-        return undefined;//from here on out all control blocks available
+        return {
+            Control: ["controls_if", "controls_repeat_ext"/*, "controls_whileUntil"*/],
+            Game: ["forward", "backward", 'turn_left', 'turn_right', 'sensor_camera'/*, 'sensor_radar'*/],
+            Logic: ["logic_compare", "logic_operation", "logic_negate", "math_arithmetic", "math_modulo"],
+            Values: ["math_number", "text", "logic_boolean", /* "math_random_int"*/],
+            //   Debug: ["debug_log"]
+        };
     }
 
     initializeState(gameState: GameState): void {
+        this.currentlyAcquiredCollectibles = 0;
         gameState.setGridState(this.initialMazeLayout);
         gameState.setPlayerPosition(new PlayerPosition(4, 3, PLAYER_ORIENTATION_RIGHT));
     }
 
-    tick(gameState: GameState): void {//make sure to remind players of having to collect both collectibles
+    tick(gameState: GameState): void {
+        const player = gameState.getPlayerPosition();
+        const standingOn = gameState.getGridTile(player);
+        switch (standingOn) {
+            case TILE_PIT:
+                gameState.setGameOver(true, false, MSG_FALLEN_IN_PIT);
+                break;
+            case TILE_COLLECTIBLE:
+                this.currentlyAcquiredCollectibles ++;
+                if (this.currentlyAcquiredCollectibles >= this.requiredCollectablesNumber) {
+                    gameState.setGameOver(true, true, "Gut gemacht! Komplexe Manöver sind für dich jetzt ein Klacks. Stelle dein Können in neuen Herausforderungen unter Beweis!");
+                }else{
+                    gameState.setGridTile(player, TILE_NEUTRAL);
+                }
+                break;
+        }
+
+        if(!gameState.isGameOver && !gameState.hasMoreCode){
+            gameState.setGameOver(true, false, `${MSG_CODE_OVER_BUT_YOU_NEED_MORE_COLLECTABLES} (${this.currentlyAcquiredCollectibles} von ${this.requiredCollectablesNumber})`)
+        }
     }
 }
